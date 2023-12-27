0e58db9de87982a5406c9e4300bb5ee030806a4b
Brian Freijomil <brianfreijomil@hotmail.com>
2023-12-27 15:44:33 -0300

    add matches service and stats service

---
+++ matches-microservice/src/main/java/com/bengalu/matchesmicroservice/MatchesMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.bengalu.matchesmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchesMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchesMicroserviceApplication.class, args);
+	}
+
+}
---
+++ stats-microservice/src/test/java/com/bengalu/statsmicroservice/StatsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bengalu.statsmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
