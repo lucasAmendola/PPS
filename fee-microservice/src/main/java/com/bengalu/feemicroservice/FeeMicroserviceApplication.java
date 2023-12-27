0e2abb7e5e18c6deaca719e0bf730b9b946af38a
Lucas Amendola <lukitasromanpiles7@gmail.com>
2023-12-27 11:21:59 -0300

    add fee microservice

---
+++ fee-microservice/src/main/java/com/bengalu/feemicroservice/FeeMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.bengalu.feemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeeMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeeMicroserviceApplication.class, args);
+	}
+
+}
---
+++ fee-microservice/src/test/java/com/bengalu/feemicroservice/FeeMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bengalu.feemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeeMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
